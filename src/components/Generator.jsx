ad1a941425be74e58b985887debc0364722cb8c4
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-09-13 00:59:46 -0500

    Generator 1st section buttons style

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from "react";
 import SectionWrapper from "./SectionWrapper";
-import { WORKOUTS } from "../utils/swoldier";
+import { SCHEMES, WORKOUTS } from "../utils/swoldier";
 
 function Header(props) {
   const { index, title, description } = props;
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex items-center justitfy-center gap-2">
+      <div className="flex items-center justify-center gap-2">
         <p className="text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400">
           {index}
         </p>
-        <h4 className="text-xlg sm:text-2xl md:text-3xl">{title}</h4>
+        <h4 className="text-xl sm:text-2xl md:text-3xl">{title}</h4>
       </div>
       <p className="text-sm sm:text-base mx-auto">{description}</p>
     </div>
   );
 }
 export default function Generator() {
-  const [ showModal, setShowModal ] = useState(false)
+  const [showModal, setShowModal] = useState(false);
+  const [poison, setPoison] = useState("individual");
+  const [muscles, setMuscles] = useState([]);
+  const [goal, setGoal] = useState("strength_power");
 
   function toggleModal() {
     setShowModal(!showModal);
@@ -30,14 +33,22 @@ export default function Generator() {
     >
       <Header
         index={"01"}
-        title={"Pick your workout"}
+        title={"Pick Your Workout"}
         description={"Choose from a variety of workouts to get started"}
       />
       <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
         {Object.keys(WORKOUTS).map((type, typeIndex) => {
           return (
             <button
-              className="bg-slate-950 border border-blue-350 duration-200 hover:border-blue-600 py-3 rounded-lg"
+              onClick={() => {
+                setPoison(type);
+              }}
+              className={
+                "bg-slate-950 border border-blue-350 duration-200 hover:border-blue-600 py-3 rounded-lg " +
+                (type === poison
+                  ? "border-blue-600 bg-blue-600 text-white"
+                  : "border-blue-200")
+              }
               key={typeIndex}
             >
               <p className="capitalize">{type.replaceAll("_", " ")}</p>
@@ -48,15 +59,46 @@ export default function Generator() {
 
       <Header
         index={"02"}
-        title={"Lock on targets"}
+        title={"Lock On Targets"}
         description={"Select the multiple targets"}
       />
       <div className="bg-slate-950 border border-solid border-blue-400 rounded-lg flex flex-col">
-        <button onClick={toggleModal} className="relative p-3 flex items-center justify-center">
+        <button
+          onClick={toggleModal}
+          className="relative p-3 flex items-center justify-center"
+        >
           <p>Select muscle groups</p>
           <i className="fa-solid absolute right-3 top-1/2 -translate-y-1/2 fa-caret-down"></i>
         </button>
         {showModal && <div>Modal</div>}
+      </div>
+
+      <Header
+        index={"03"}
+        title={"Select Your Primary Fitness Focus"}
+        description={
+          "Please choose one of the following options that best aligns with your current fitness goals"
+        }
+      />
+      <div className="grid grid-cols-3 gap-4">
+        {Object.keys(SCHEMES).map((scheme, schemeIndex) => {
+          return (
+            <button
+              onClick={() => {
+                setGoal(scheme);
+              }}
+              className={
+                "bg-slate-950 border border-blue-350 duration-200 hover:border-blue-600 py-3 rounded-lg " +
+                (scheme === goal
+                  ? "border-blue-600 bg-blue-600 text-white"
+                  : "border-blue-200")
+              }
+              key={schemeIndex}
+            >
+              <p className="capitalize">{scheme.replaceAll("_", " ")}</p>
+            </button>
+          );
+        })}
       </div>
     </SectionWrapper>
   );
